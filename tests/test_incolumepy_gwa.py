9d9a03cdaab8d7d14ff9576f8d2dcb905edf7125
Britodfbr <britodfbr@gmail.com>
2021-12-10 20:01:58 -0300

    lint corretions

--- tests/test_incolumepy_gwa.py
+++ tests/test_incolumepy_gwa.py
@@ -4,12 +4,27 @@ import pytest
 
 from incolumepy.gwa import __root__, __version__, version_file
 
-__author__ = '@britodfbr'  # pragma: no cover
+__author__ = "@britodfbr"  # pragma: no cover
+
+
 def test_root():
-    assert '/'.join(__root__.parts[-3:]) == "incolumepy.gwa/incolumepy/gwa"
+    assert "/".join(__root__.parts[-3:]) == "incolumepy.gwa/incolumepy/gwa"
+
+
 def test_version_file():
-    assert version_file.name == 'version.txt'
+    assert version_file.name == "version.txt"
+
+
 def test_version():
     assert re.fullmatch(r"\d+(.\d+){2}(-\w+.\d+)?", __version__, flags=re.I)
+
+
+@pytest.mark.parametrize(
+    "name, greater, expected",
+    ["", False, "Hello Visitor."],
+    ["", True, "Hello Visitor!"],
+    ["Ada", False, "Hello Ada."],
+    ["Ana", True, "Hello Ana!"],
+)
 def test_hello():
     ...

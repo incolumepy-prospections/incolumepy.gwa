72b3203e79fe4d3c94aeabf8ab3fe1daa71233fd
Britodfbr <britodfbr@gmail.com>
2021-12-10 13:30:12 -0300

    Version control with poetry + pyproject.toml

---
+++ incolumepy/gwa/__init__.py
@@ -0,0 +1,9 @@
+import toml
+from pathlib import Path
+__root__ = Path(__file__).parents[0]
+version_file = __root__.joinpath('version.txt')
+version_file.write_text(f"{toml.load(Path(__file__).parents[2].joinpath('pyproject.toml'))['tool']['poetry']['version']}\n")
+__version__ = version_file.read_text().strip()
+if __name__ == '__main__':
+    print(__root__)
+    print(__version__)
--- tests/test_incolumepy_gwa.py
+++ tests/test_incolumepy_gwa.py
@@ -1,5 +1,12 @@
 import pytest
-
+import re
+from incolumepy.gwa import __version__, __root__, version_file
 __author__ = '@britodfbr'  # pragma: no cover
-
+def test_root():
+    assert '/'.join(__root__.parts[-3:]) == "incolumepy.gwa/incolumepy/gwa"
+def test_version_file():
+    assert version_file.name == 'version.txt'
+def test_version():
+    assert re.fullmatch(r"\d+(.\d+){2}(-\w+.\d+)?", __version__, flags=re.I)
 def test_hello():
+    ...
